192b50924f253d6c824cd45f128e5ba734f42bc3
Ivan Karlo <i.karlo@outlook.com>
2026-06-26 12:20:40 +0300

    refactor(serviceFiles): вынесли URL схем vanessa-runner в schemaUrls.ts и дефолты env в envDefaults.ts

---
+++ src/features/serviceFiles/envDefaults.ts
@@ -0,0 +1,53 @@
+import { VRUNNER_SCHEMA } from './schemaUrls';
+
+/** Базовые поля секции default, общие для всех env-файлов */
+const DEFAULT_SECTION = {
+	'--ibconnection': '/F./build/ib',
+	'--db-user': '',
+	'--db-pwd': '',
+	'--root': '.',
+	'--workspace': '.',
+	'--v8version': '8.3',
+	'--locale': 'ru',
+	'--language': 'ru',
+};
+
+/** Канонический дефолт env.json */
+export const ENV_DEFAULTS = {
+	$schema: VRUNNER_SCHEMA,
+	default: {
+		...DEFAULT_SECTION,
+		'--additional': '/DisplayAllFunctions /L ru',
+		'--ordinaryapp': '-1',
+	},
+};
+
+const VANESSA_ADDITIONAL = '/DisplayAllFunctions /L ru';
+
+/** Канонический дефолт tools/vrunner.json */
+export const VRUNNER_DEFAULTS = {
+	$schema: VRUNNER_SCHEMA,
+	default: {
+		...DEFAULT_SECTION,
+		'--ordinaryapp': '0',
+	},
+	vanessa: {
+		'--vanessasettings': './tools/VAParams.json',
+		'--pathvanessa': './oscript_modules/vanessa-automation-single/vanessa-automation-single.epf',
+		'--additional': VANESSA_ADDITIONAL,
+	},
+};
+
+/** Канонический дефолт tools/vrunner.init.json */
+export const VRUNNER_INIT_DEFAULTS = {
+	$schema: VRUNNER_SCHEMA,
+	default: {
+		...DEFAULT_SECTION,
+		'--ordinaryapp': '0',
+	},
+	vanessa: {
+		'--vanessasettings': './tools/VAParams.init.json',
+		'--pathvanessa': './oscript_modules/vanessa-automation-single/vanessa-automation-single.epf',
+		'--additional': VANESSA_ADDITIONAL,
+	},
+};
